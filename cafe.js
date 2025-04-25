22dbabbd4809fa8e679ab4c2a2e60f1d89c79e6f
ShernOC <sherlynea1994@hotmail.com>
2025-04-25 12:23:07 +0300

    add order function

---
+++ cafe.js
@@ -0,0 +1,18 @@
+// Order to be done
+function order(order){
+  switch (order){
+    case "Kenyan Tea - 290":
+      return order 
+    case "Kenyan Tea - 290":
+      return order
+  }
+
+}
+
+function coffeeOrder(order){
+  if (order === "Kenyan Tea - 290"){
+    return order
+  }else if (order ===  "Tangawizi Tea- 290"){
+    return order
+  }
+}66-]
