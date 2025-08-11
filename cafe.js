8a40d54681f7df232d3e66ab1a9786322fb48fa0
ShernOC <sherlynea1994@hotmail.com>
2025-08-11 14:54:31 +0300

    update js

--- cafe.js
+++ cafe.js
@@ -1,28 +1,61 @@
-// Order to be done
+// // created an 
+//     let drinkType =''; 
+//     let pastryType ='';
 
-function priceOrder(){
-  const 
-  switch (order){
-    case "Kenyan Tea - 290":
-      return order 
-    case "Kenyan Tea - 290":
-      return order
-  }
+//     function driOrder(drinkType){
+//         if (drinkType === 'Double espresso'){
+//             alert("You have ordered Double espresso")
+//         }else if(drinkType === 'Cappuccino'){
+//             alert("You have ordered Cappuccino")
+            
+//         }else if(drinkType === 'Latte'){
+//             alert("You have ordered Latte")
 
-}
+//         }else if(drinkType === 'Hazelnut Mocha'){
+//             alert("You have ordered Hazelnut Mocha")
+//         }else if(drinkType === 'Single Coffee'){
+//             alert("You have ordered Single Coffee")
+//         }else if(drinkType === 'Iced Coffee'){
+//             alert("You have ordered Iced Coffee")
+//         }else if(drinkType === 'Kenya Tea'){
+//             alert("You have ordered Cappuccino")
+//         }else if(drinkType === 'Tangawizi Tea'){
+//             alert("You have ordered Tangawizi")
+//         }else if (drinkType==='Masala Tea'){
+//             alert("You have ordered Masala Tea")
+//         }else if(drinkType === 'Dawa'){
+//             alert('You have ordered Dawa')
+//         }else if (drinkType === 'Chamomile Tea'){
+//             alert('You have ordered Chamomile Tea')
+//         }else if (drinkType === 'Hot Chocolate '){
+//             alert ('YOu have ordered Hot Chocolate')
+//         }else{
+//             alert("Make an order ")
+//         }
+//     }
+
+//     function pastryOrder(pastryType){
+//         if (pastryType === 'Butter Croissant'){
+//             alert("You have ordered Butter Croissant")
+//         }else if(pastryType === 'Chicken Pie'){
+//             alert("You have ordered Chicken Pie")
+//         }else if(pastryType === 'Samosas'){
+//             alert("You have ordered Samosas")
+//         }else if(pastryType === 'Burger with Fries'){
+//             alert("You have ordered Burger with Fries")
+//         }else if(pastryType === 'Mandazis'){
+//             alert("You have ordered Mandazis")
+//         }else if(pastryType === 'Eggs and Toast'){
+//             alert("You have ordered Eggs and Toast")
+//         }else {alert('Order your toast')}
+//     }
 
 
-// Order the coffee
-let order = '';
-function makeAnOrder(order){
-  if (order === "Kenyan Tea"){
-    if(price =  ){
 
-    }
-    return order
-  }else if (order ===  "Tangawizi Tea- 290"){
-    return order
-  }
-}
 
-// We can use the let 
+
+const logoElement = document.querySelector('.js-logo').innerHTML='SHERNS COFEE SHOP'
+
+console.log(logoElement)
+
+
---
+++ cafe1.js
@@ -0,0 +1,27 @@
+// Order to be done
+
+function priceOrder(){
+  switch (order){
+    case "Kenyan Tea - 290":
+      return order 
+    case "Kenyan Tea - 290":
+      return order
+  }
+
+}
+
+
+// Order the coffee
+let order = '';
+function makeAnOrder(order){
+  if (order === "Kenyan Tea"){
+    if(price === "kenyan tea "){
+
+    }
+    return order
+  }else if (order ===  "Tangawizi Tea- 290"){
+    return order
+  }
+}
+
+// We can use the let 
