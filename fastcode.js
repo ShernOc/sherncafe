58110dbda591e888e85c979ddc9ed5353314a37a
ShernOC <sherlynea1994@hotmail.com>
2025-08-12 15:35:12 +0300

    remove script

---
+++ fastcode.js
@@ -0,0 +1,51 @@
+  // Drink Order
+
+    function driOrder(drinkType){
+        if(drinkType === 'Double espresso'){
+                orderBtn.innerText === 'Ordered'
+            }else if(drinkType === 'Cappuccino'){
+               orderBtn.innerText = 'Ordered'
+
+            }else if(drinkType === 'Latte'){
+               orderBtn.innerText === 'Ordered'
+                
+            }else if(drinkType === 'Hazelnut Mocha'){
+               orderBtn.innerText === 'Ordered'
+            }else if(drinkType === 'Single Coffee'){
+              orderBtn.innerText === 'Ordered'
+            }else if(drinkType === 'Iced Coffee'){
+                orderBtn.innerText === 'Ordered'
+            }else if(drinkType === 'Kenya Tea'){
+                orderBtn.innerText === 'Ordered'
+            }else if(drinkType === 'Tangawizi Tea'){
+                orderBtn.innerText === 'Ordered'
+            }else if (drinkType==='Masala Tea'){
+                orderBtn.innerText === 'Ordered'
+            }else if(drinkType === 'Dawa'){
+                orderBtn.innerText === 'Ordered'
+            }else if (drinkType === 'Chamomile Tea'){
+                orderBtn.innerText === 'Ordered'
+            }else if (drinkType === 'Hot Chocolate '){
+                orderBtn.innerText === 'Ordered'
+            }
+            else
+            {alert("What am i doing?")}
+        }
+
+  
+  // Pastry Order 
+  function pastryOrder(pastryType){
+        if (pastryType === 'Butter Croissant'){
+            alert("You have ordered Butter Croissant")
+        }else if(pastryType === 'Chicken Pie'){
+            alert("You have ordered Chicken Pie")
+        }else if(pastryType === 'Samosas'){
+            alert("You have ordered Samosas")
+        }else if(pastryType === 'Burger with Fries'){
+            alert("You have ordered Burger with Fries")
+        }else if(pastryType === 'Mandazis'){
+            alert("You have ordered Mandazis")
+        }else if(pastryType === 'Eggs and Toast'){
+            alert("You have ordered Eggs and Toast")
+        }else {alert('Order your toast')}
+    }
