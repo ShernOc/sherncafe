151662866186b0700ec8bb5bd0216570deccf3f9
ShernOC <sherlynea1994@hotmail.com>
2025-08-15 17:24:01 +0300

    made payment

--- cafe.js
+++ cafe.js
@@ -1,11 +1,59 @@
 
+// index.html
+
+// Goal to be able to change the text of all the buttons to Ordered once the button is clicked, 
+// Once the button is clicked with the right amount it altered to ordered. 
+
+    let drinkType ='';  
+    let pastryType ='';
+
+    function driOrder(drinkType){
+        const orderBtn=document.querySelector('#drink1');
+        const orderBtn2=document.querySelector('#drink2');
+        
+        if (orderBtn.innerText === 'Order Now'){
+            orderBtn.innerText = 'Ordered'
+        }else{
+            orderBtn.innerText = 'Order Now'
+        }
+        
+    }
+
+   function pastryOrder(pastryType){
+        const pastries=document.querySelector('#pastry1');
+        
+        if (pastries.innerText === 'Order Now'){
+            pastries.innerText = 'Ordered'
+        }else{
+            pastries.innerText = 'Order Now'
+        }
+    }
 
 
+// Order.html: 
+// Make Payments : 
+    function makeOrder(){
+        const orderTotal = document.querySelector('.js-input-orders');
+
+        let orders = Number(orderTotal.value);
+        console.log(orders)
+        
+        //Order Calculations
+        if(orders >=200){
+            orders = orders + (orders*0.1)
+        }
+
+    // total input 
+    document.querySelector('.js-total-cost').innerHTML = `$ ${orders}`;
+}
+
+// input event 
+function handleKeydown(event){
+    if(event.key ==='Enter'){
+        makeOrder();
+    }
+}
 
 
-
-const logoElement = document.querySelector('.js-logo').innerHTML='SHERNS COFEE SHOP'
-
-console.log(logoElement)
 
 
